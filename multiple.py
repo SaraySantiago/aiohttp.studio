16c0a07ab74db19a43a7a937864cc5a21cf3a58b
SaraySantiago <ssantiago18@iesrioarba.es>
2024-11-07 13:14:27 +0100

    .

--- multiple.py
+++ multiple.py
@@ -10,15 +10,19 @@ async def get_async(url):
 
 
 urls = ['http://webcode.me', 'https://httpbin.org/get',
-    'https://google.com', 'https://stackoverflow.com',
+    'https://google.com','https://tusalchichonfavorito.com',
+    'https://stackoverflow.com',
     'https://github.com']
 
 
 async def launch():
     """Lanzadera lanzable asincrona que pide como un pobre"""
-    
+    #TODO: intentar poner un try// except -> ante una url inexistente
+    #try:
     resps = await asyncio.gather(*map(get_async, urls)) # -> TODO: entender porque esta esa *
     stat = [resp.status for resp in resps]
+    #except #socket,gaierror
+    #except #ClientConnectorError 
 
     for status_code in stat:
         print(status_code)
